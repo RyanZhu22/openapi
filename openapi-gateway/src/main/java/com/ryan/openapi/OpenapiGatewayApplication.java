59b97660e6d83bf362cf8fcfea6d988b18fc8bd6
ryanzhu <ryanzhu.work@gmail.com>
2023-06-26 21:06:11 +0800

    update: gateway project

--- openapi-gateway/src/main/java/com/ryan/openapi/OpenapiGatewayApplication.java
+++ openapi-gateway/src/main/java/com/ryan/openapi/OpenapiGatewayApplication.java
@@ -5,8 +5,11 @@ import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Service;
+
+import java.io.File;
 
 @Service
 @EnableDubbo
@@ -17,6 +20,15 @@ public class OpenapiGatewayApplication {
     private DemoService demoService;
 
     public static void main(String[] args) {
+        ApplicationHome home = new ApplicationHome(OpenapiGatewayApplication.class);
+        File jarFile = home.getSource();
+        String dirPath = jarFile.getParentFile().toString();
+        String filePath = dirPath + File.separator + ".dubbo";
+        System.out.println(filePath);
+
+        System.setProperty("dubbo.meta.cache.filePath", filePath);
+        System.setProperty("dubbo.mapping.cache.filePath",filePath);
+
         ConfigurableApplicationContext context = SpringApplication.run(OpenapiGatewayApplication.class, args);
         OpenapiGatewayApplication application = context.getBean(OpenapiGatewayApplication.class);
         String result = application.doSayHello("Hello");
