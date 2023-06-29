a73ddea23fa3bf6e63d9443ac8c6099bbc7676b4
ryanzhu <ryanzhu.work@gmail.com>
2023-06-29 21:20:03 +0800

    update: comment wx related code

--- openapi-backend/src/main/java/com/ryan/openapi/controller/UserController.java
+++ openapi-backend/src/main/java/com/ryan/openapi/controller/UserController.java
@@ -1,47 +1,31 @@
 package com.ryan.openapi.controller;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.ryan.openapi.constant.UserConstant;
-import com.ryan.openapi.model.vo.LoginUserVO;
-import com.ryan.openapi.model.vo.UserVO;
 import com.ryan.openapi.annotation.AuthCheck;
 import com.ryan.openapi.common.BaseResponse;
 import com.ryan.openapi.common.DeleteRequest;
 import com.ryan.openapi.common.ErrorCode;
 import com.ryan.openapi.common.ResultUtils;
-import com.ryan.openapi.config.WxOpenConfig;
+import com.ryan.openapi.common.model.entity.User;
+import com.ryan.openapi.constant.UserConstant;
 import com.ryan.openapi.exception.BusinessException;
 import com.ryan.openapi.exception.ThrowUtils;
-import com.ryan.openapi.model.dto.user.UserAddRequest;
-import com.ryan.openapi.model.dto.user.UserLoginRequest;
-import com.ryan.openapi.model.dto.user.UserQueryRequest;
-import com.ryan.openapi.model.dto.user.UserRegisterRequest;
-import com.ryan.openapi.model.dto.user.UserUpdateMyRequest;
-import com.ryan.openapi.model.dto.user.UserUpdateRequest;
-import com.ryan.openapi.model.entity.User;
+import com.ryan.openapi.model.dto.user.*;
+import com.ryan.openapi.model.vo.LoginUserVO;
+import com.ryan.openapi.model.vo.UserVO;
 import com.ryan.openapi.service.UserService;
-import java.util.List;
-import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import me.chanjar.weixin.common.bean.WxOAuth2UserInfo;
-import me.chanjar.weixin.common.bean.oauth2.WxOAuth2AccessToken;
-import me.chanjar.weixin.mp.api.WxMpService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.BeanUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 /**
  * 用户接口
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
  */
 @RestController
 @RequestMapping("/user")
@@ -51,8 +35,8 @@ public class UserController {
     @Resource
     private UserService userService;
 
-    @Resource
-    private WxOpenConfig wxOpenConfig;
+//    @Resource
+//    private WxOpenConfig wxOpenConfig;
 
     // region 登录相关
 
@@ -98,28 +82,28 @@ public class UserController {
         return ResultUtils.success(loginUserVO);
     }
 
-    /**
-     * 用户登录（微信开放平台）
-     */
-    @GetMapping("/login/wx_open")
-    public BaseResponse<LoginUserVO> userLoginByWxOpen(HttpServletRequest request, HttpServletResponse response,
-            @RequestParam("code") String code) {
-        WxOAuth2AccessToken accessToken;
-        try {
-            WxMpService wxService = wxOpenConfig.getWxMpService();
-            accessToken = wxService.getOAuth2Service().getAccessToken(code);
-            WxOAuth2UserInfo userInfo = wxService.getOAuth2Service().getUserInfo(accessToken, code);
-            String unionId = userInfo.getUnionId();
-            String mpOpenId = userInfo.getOpenid();
-            if (StringUtils.isAnyBlank(unionId, mpOpenId)) {
-                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "登录失败，系统错误");
-            }
-            return ResultUtils.success(userService.userLoginByMpOpen(userInfo, request));
-        } catch (Exception e) {
-            log.error("userLoginByWxOpen error", e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "登录失败，系统错误");
-        }
-    }
+//    /**
+//     * 用户登录（微信开放平台）
+//     */
+//    @GetMapping("/login/wx_open")
+//    public BaseResponse<LoginUserVO> userLoginByWxOpen(HttpServletRequest request, HttpServletResponse response,
+//            @RequestParam("code") String code) {
+//        WxOAuth2AccessToken accessToken;
+//        try {
+//            WxMpService wxService = wxOpenConfig.getWxMpService();
+//            accessToken = wxService.getOAuth2Service().getAccessToken(code);
+//            WxOAuth2UserInfo userInfo = wxService.getOAuth2Service().getUserInfo(accessToken, code);
+//            String unionId = userInfo.getUnionId();
+//            String mpOpenId = userInfo.getOpenid();
+//            if (StringUtils.isAnyBlank(unionId, mpOpenId)) {
+//                throw new BusinessException(ErrorCode.SYSTEM_ERROR, "登录失败，系统错误");
+//            }
+//            return ResultUtils.success(userService.userLoginByMpOpen(userInfo, request));
+//        } catch (Exception e) {
+//            log.error("userLoginByWxOpen error", e);
+//            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "登录失败，系统错误");
+//        }
+//    }
 
     /**
      * 用户注销
