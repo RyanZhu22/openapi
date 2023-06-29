f710f472fdf2f3646ba6b98953a35ca78d44c926
ryanzhu <ryanzhu.work@gmail.com>
2023-06-29 21:22:42 +0800

    update: clean interface project

--- openapi-interface/src/main/java/com/ryan/openapiinterface/controller/NameController.java
+++ openapi-interface/src/main/java/com/ryan/openapiinterface/controller/NameController.java
@@ -27,42 +27,7 @@ public class NameController {
 
     @PostMapping("/user")
     public String getUsernameByPost(@RequestBody User user, HttpServletRequest request) {
-        String accessKey = request.getHeader("accessKey");
-        String nonce = request.getHeader("nonce");
-        String timestamp = request.getHeader("timestamp");
-        String sign = request.getHeader("sign");
-        String body = request.getHeader("body");
-
-        // TODO 实际情况从数据库中查是否已分配给用户
-        if (!accessKey.equals("ryan")) {
-            throw new RuntimeException("未授权");
-        }
-        // TODO 这个出错
-        if (Long.parseLong(nonce) > 10000) {
-            throw new RuntimeException("未授权");
-        }
-        // TODO 时间和当前时间不超过五分钟
-//        if (timestamp) {
-//
-//        }
-        // Get current instant.
-        long currentTimestamp = System.currentTimeMillis() / 1000;
-        System.out.println("CURRENTINSTANT: " + currentTimestamp);
-        // Check if the timestamp is more than 5 minutes ahead of current time.
-        long headerTimestamp = Long.parseLong(timestamp);
-        System.out.println(headerTimestamp);
-        if ((currentTimestamp - headerTimestamp) > 300) {
-            throw new RuntimeException("Timestamp is more than 5 minutes ahead");
-        }
-        // TODO 实际情况中是从数据库中查出secretKey
-//        com.ryan.openapiinterface.model.User interfaceUser = userService.selectByAccessKey(accessKey);
-//        String serverSign = SignUtils.getSign(body, interfaceUser.getSecretKey());
-//        if (!sign.equals(serverSign)) {
-//            throw new RuntimeException("无权限");
-//        }
-        // TODO count + 1 after invoking
         String result = "POST 你的名字是" + user.getUsername();
-
         return result;
     }
 }
